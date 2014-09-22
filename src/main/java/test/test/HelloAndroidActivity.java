b223b752726e43fe93635ccd795739700bc00c71
WonderCsabo <kozakcsabi@gmail.com>
2014-09-22 17:39:41 +0200

    Initial commit

---
+++ src/main/java/test/test/HelloAndroidActivity.java
@@ -0,0 +1,18 @@
+package test.test;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.RoboGuice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+@RoboGuice
+@EActivity(R.layout.activity_main)
+public class HelloAndroidActivity extends Activity {
+
+ 
+
+}
+
